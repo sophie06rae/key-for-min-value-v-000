240a1dda3d6fe11c6725cb10499c876f25168c8f
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-18 12:07:54 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil
-    lowest_value = nil
+  lowest_value = nil
     name_hash.each do |k, v|
       if lowest_value == nil || v < lowest_value
         lowest_value = v
